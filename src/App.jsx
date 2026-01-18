043dce7fc976d7b64d290e7c3b4acc5fbf57d46d
Joel Nakabayashi <joelnakabayashi@new-host-3.home>
2026-01-18 14:34:56 -0800

    Wire Home receiver

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import AppLayout from "./layouts/AppLayout.jsx";
-import Product from "./pages/Product.jsx";
+import Home from "./pages/Home.jsx";
 import Shop from "./pages/Shop.jsx";
-
-function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Home</h1>
-    </div>
-  );
-}
+import Product from "./pages/Product.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/product/:shareId" element={<Product />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:shareId" element={<Product />} />
+        <Route path="/account" element={<div style={{ padding: 24 }}>Account (placeholder)</div>} />
+        <Route path="/login" element={<div style={{ padding: 24 }}>Login (placeholder)</div>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
